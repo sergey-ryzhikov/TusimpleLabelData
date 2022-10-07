1628860f92b591257d15da7b03f76336d67481a0
Sergey Ryzhikov <sergey-werk@ya.ru>
2022-10-08 00:23:20 +0300

    fix tabulation

--- label_data.py
+++ label_data.py
@@ -27,35 +27,35 @@ class LabelData(dict):
         return json.dumps({k: getattr(self, k, None) for k in attrs})
 
     def to_relative(self, dim_lanes, dim_h_samples, round_=2):
-      """ Convert absolute coordinates to relative (in %). 
-      """
-      assert 'relative' not in self or not self['relative'], "Coordinates are relative already."
-      assert dim_lanes >= self.max_lanes, "The 'dim_lanes' is less than the current maximum."
-      assert dim_h_samples >= self.max_h_samples, "The 'dim_h_samples' is less than the current maximum."
-      
-      return self.__class__(
-          h_samples=[round(x / dim_h_samples * 100, round_) for x in self['h_samples']],
-          lanes=[[round(x / dim_lanes * 100, round_) if x > 0 else x
-                          for x in lane]
-                          for lane in self['lanes']],
-          raw_file=self['raw_file'],
-          relative=True
-        )
+        """ Convert absolute coordinates to relative (in %). 
+        """
+        assert 'relative' not in self or not self['relative'], "Coordinates are relative already."
+        assert dim_lanes >= self.max_lanes, "The 'dim_lanes' is less than the current maximum."
+        assert dim_h_samples >= self.max_h_samples, "The 'dim_h_samples' is less than the current maximum."
+        
+        return self.__class__(
+            h_samples=[round(x / dim_h_samples * 100, round_) for x in self['h_samples']],
+            lanes=[[round(x / dim_lanes * 100, round_) if x > 0 else x
+                            for x in lane]
+                            for lane in self['lanes']],
+            raw_file=self['raw_file'],
+            relative=True
+            )
 
     def to_absolute(self, dim_lanes, dim_h_samples):
-      """ Convert relative coordinates to absolute ones. """
-      assert self['relative'] == True, "Coordinates are absolute."
-      assert self.max_lanes <= 100, f"Relative value for max_lanes is invalid:{self.max_lanes}"
-      assert self.max_h_samples <= 100, f"Relative value for h_samples is invalid:{self.max_h_samples}"
+        """ Convert relative coordinates to absolute ones. """
+        assert self['relative'] == True, "Coordinates are absolute."
+        assert self.max_lanes <= 100, f"Relative value for max_lanes is invalid:{self.max_lanes}"
+        assert self.max_h_samples <= 100, f"Relative value for h_samples is invalid:{self.max_h_samples}"
 
-      return self.__class__(
-          h_samples=[round(x / 100 * dim_h_samples) for x in self['h_samples']],
-          lanes=[[round(x / 100 * dim_lanes) if x > 0 else x
-                          for x in lane]
-                          for lane in self['lanes']],
-          raw_file=self['raw_file'],
-          relative=None
-        )
+        return self.__class__(
+            h_samples=[round(x / 100 * dim_h_samples) for x in self['h_samples']],
+            lanes=[[round(x / 100 * dim_lanes) if x > 0 else x
+                            for x in lane]
+                            for lane in self['lanes']],
+            raw_file=self['raw_file'],
+            relative=None
+            )
 
     @property
     def max_lanes(self):
