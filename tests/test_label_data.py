97339e33ba3bc68f65e40ee90d1e9303ed747eb8
Sergey Ryzhikov <serg.ryzh.ml@gmail.com>
2022-10-10 10:36:40 +0300

    cleanup

--- tests/test_label_data.py
+++ tests/test_label_data.py
@@ -8,22 +8,29 @@ from pathlib import Path
 sys.path.append(str(Path(__file__).resolve().parent.parent))
 from label_data import LabelData
 
-dict_ok = dict(lanes=[[-2, 1, 2], [1, 2, 3]], h_samples=[1, 2, 3], raw_file="aaa/bbb/01.jpg")
 json_ok = '{"lanes": [[-2, 1, 2], [1, 2, 3]], "h_samples": [1, 2, 3], "raw_file": "aaa/bbb/01.jpg"}'
 json_ok_no_lanes = '{"lanes": [], "h_samples": [1, 2, 3], "raw_file": "aaa/bbb/01.jpg"}'
 
+dict_ok = json.loads(json_ok)
+
+
 def test_from_json():
-    LabelData.from_json(json_ok)
+    assert LabelData(**dict_ok) == LabelData.from_json(json_ok), "wrong"
     LabelData.from_json(json_ok_no_lanes)
 
 def test_to_json():
     res = LabelData(**dict_ok).to_json()
-    assert res == json_ok, f"wrong {res}"
+    assert res == json_ok, f"wrong"
 
 def test_rerp():
     json_ok_repr = "LabelData(raw_file='aaa/bbb/01.jpg')"
-    repr = str(LabelData(**dict_ok))
+    repr = LabelData(**dict_ok).__repr__()
     assert repr == json_ok_repr, f"wrong __repr__: {repr}"
+
+def test_str():
+    json_ = LabelData(**dict_ok).to_json()
+    str_ =  str(LabelData(**dict_ok))
+    assert str_ == json_, f"wrong __str__: {str_}"
 
 def test_len():
     n = len(LabelData(**dict_ok)['lanes'])
@@ -33,17 +40,13 @@ def test_del():
     item = LabelData(**dict_ok)
     del item['lanes']
 
-def test_crop_px():
-    test1 = '{"lanes": [[-2, 3, 2, 1], [3, 4, 5, -2]], "h_samples": [1, 2, 3, 4]}'
-    assert LabelData.from_json(test1).crop(top=2) == \
-        '{"lanes": [[3, 2, 1], [4, 5, -2]], "h_samples": [1, 2, 3]}'
-
 def test_rel():
-    item = LabelData.from_json('{"lanes": [[-2, 51]], "h_samples": [1]}')
-    item_rel = item.get_relative(100,100)
-    assert item == item_rel.to_absolute(100,100), "wrong"
-    assert item != item_rel.to_absolute(99,100), "wrong 99"
-    assert item != item_rel.to_absolute(101,100), "wrong 101"
+    item = LabelData(**{'lanes': [[-2, 51]], 'h_samples': [1]})
+    item_rel = item.to_relative(100, 100, round_=3)
+
+    assert item == item_rel.to_absolute(100, 100), "wrong 100"
+    assert item != item_rel.to_absolute(99, 100), f"wrong 99"
+    assert item != item_rel.to_absolute(101, 100), f"wrong 101"
     pass
 
 
