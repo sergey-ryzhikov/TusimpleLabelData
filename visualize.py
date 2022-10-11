ce0a3e9ee724455b415d4e6771f1b079d0244d56
Sergey Ryzhikov <serg.ryzh.ml@gmail.com>
2022-10-11 22:37:05 +0300

    sort lanes before drawing

--- visualize.py
+++ visualize.py
@@ -2,6 +2,7 @@
 import sys
 from pathlib import Path
 from matplotlib import pyplot as plt
+import numpy as np
 
 if __name__ == '__main__':
     sys.path.append(str(Path(__file__).resolve().parent.parent))
@@ -17,7 +18,9 @@ def plot_image(img, label, **plot_args):
 
     plt.imshow(img.detach().permute(1,2,0))
 
-    for i, lane in enumerate(label.lanes):
+
+    # Lanes in dataset are not in order, so sort them left to right
+    for i, lane in enumerate(sorted(label.lanes, key=lambda lane: np.nanmean(lane))):
         color = colors[i % len(colors)]
         plt.plot(lane, label.h_samples, 
                 **{'color': color, **plot_args}  # use default color if not specified
@@ -32,5 +35,12 @@ if __name__ == "__main__":
     dataset = TusimpleDataset(sample_root, resize_to=(256, 512))
 
     img, label = dataset[7]
-    plot_image(img, label)
+    plot_image(img, label, linewidth=10, alpha=0.5)
+
     plt.show()
+
+
+
+# #TODO: 
+# from torchvision.utils import draw_segmentation_masks
+# img = draw_segmentation_masks(img, mask, alpha=0.8, colors="blue"))
