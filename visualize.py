41ef5a816c119a1e8fac8c276ff28feaccc16ff9
Sergey Ryzhikov <serg.ryzh.ml@gmail.com>
2022-10-11 20:32:14 +0300

    visualize

---
+++ visualize.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+import sys
+from pathlib import Path
+from matplotlib import pyplot as plt
+
+if __name__ == '__main__':
+    sys.path.append(str(Path(__file__).resolve().parent.parent))
+    from TusimpleUtils import TusimpleDataset
+else:
+    from .dataset import TusimpleDataset
+
+
+def plot_image(img, label):
+    plt.imshow(img.detach().permute(1,2,0))
+    for lane in label.lanes:
+        plt.plot(lane, label.h_samples)
+
+if __name__ == "__main__":
+
+    current_dir = Path(__file__).parent
+    sample_root = (current_dir / 'tusimple_sample_512x288/').resolve()
+
+    dataset = TusimpleDataset(sample_root, resize_to=(256, 512))
+
+    img, label = dataset[0]
+    plot_image(img, label)
+    plt.show()
