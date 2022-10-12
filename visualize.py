1a19926f0a9b638aca12597af3eae970b391e474
Sergey Ryzhikov <serg.ryzh.ml@gmail.com>
2022-10-12 15:25:31 +0300

    plot_image: add default arguments

--- visualize.py
+++ visualize.py
@@ -11,7 +11,7 @@ else:
     from .dataset import TusimpleDataset
 
 
-def plot_image(img, label, numbers=True, fontsize=12, **plot_args):
+def plot_image(img, label, numbers=True, linewidth=10, alpha=0.4, fontsize=12, solid_capstyle='round', **plot_args):
 
     prop_cycle = plt.rcParams['axes.prop_cycle']  # default colors
     colors = prop_cycle.by_key()['color']
@@ -20,19 +20,21 @@ def plot_image(img, label, numbers=True, fontsize=12, **plot_args):
 
     c, height, width = img.shape
     ax.imshow(img.detach().permute(1,2,0))
-    
 
     # Lanes in dataset are not in order, so sort them left to right
     lanes_sorted = label.lanes_sorted
 
     for i, lane in enumerate(lanes_sorted):
-        color = colors[i % len(colors)]
-
-        args = {'color': color, **plot_args}  # use default colormap if color is not specified
         # TODO: accept an iterable of colors
-
+        color = colors[i % len(colors)]
+        args = {'color': color, **plot_args}  # use default colormap if color is not specified
+        
         # plot line
-        plt.plot(lane, label.h_samples, **args)
+        plt.plot(lane, label.h_samples,
+                        linewidth=linewidth,
+                        alpha=alpha,
+                        solid_capstyle=solid_capstyle,
+                        **args)
 
         # plot number of line
         if numbers :
@@ -67,7 +69,7 @@ if __name__ == "__main__":
     dataset = TusimpleDataset(sample_root, resize_to=(256, 512))
 
     img, label = dataset[9]
-    plot_image(img, label, linewidth=10, alpha=0.5, solid_capstyle='round' )
+    plot_image(img, label)
 
     plt.show()
 
